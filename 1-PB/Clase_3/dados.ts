c7c386ba00e64d17caa579d24e3c84e4aea713a6
LarsenSonia <larsen.sonia.graciela@gmail.com>
2022-05-26 19:49:00 -0300

    main

--- 1-PB/Clase_3/dados.ts
+++ 1-PB/Clase_3/dados.ts
@@ -1,5 +1,5 @@
 import * as rls from 'readline-sync';
-let cantDados: number;
+let cantDados: number;  //esta mal
 cantDados = rls.questionInt("Un dado,1/6 probabilidades de sacar 6. Escriba la cantidad de dados: ");
 let n : number= 6**cantDados;
 console.log("Con", cantDados,"dados tiene 1/"+n+" de probabilidades de sacar 6.")
