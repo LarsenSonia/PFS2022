51ad4c02a6cfb44df29ec2097367f10daf49eba8
LarsenSonia <105574827+LarsenSonia@users.noreply.github.com>
2022-07-03 12:38:05 -0300

    3 de julio 2022 ejercicios

---
+++ 2-FE/Clase_5 2FE/dinamico/indexJavascript.js
@@ -0,0 +1,8 @@
+let boton;
+boton=document.getElementById("btnSaludar");
+boton.addEventListener("click", hacerSaludo);
+function hacerSaludo() {
+    console.log('HOLA!!');
+    alert('Hola mundo');
+};
+let con
